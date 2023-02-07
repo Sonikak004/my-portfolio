eaaadab8409c3250316a68cb5420770aae2d4dca
Abdus Sattar <sasorganicagro@gmail.com>
2023-02-07 14:40:29 +0600

    Fix menu navigation and disappearing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
  // hamburger menu open
  function hambar() {
     document.querySelector("#mobile").style.display = "block";
+
   }
 
 // hamburger menu close
@@ -9,3 +10,6 @@
     document.getElementById('mobile').style.display = 'none';
   }
   document.getElementById('close-menu').addEventListener('click', close);
+  document.querySelectorAll('.menuItems').forEach((item)=>{
+      item.addEventListener('click',close)
+    })

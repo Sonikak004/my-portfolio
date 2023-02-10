4b792ece2231962b8862780b43d47437bed8e803
sonika <sonikak004@gmail.com>
2023-02-10 19:37:18 +0100

    Make change in the project section

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.querySelectorAll('.menuItems').forEach((item) => {
 const proCards = [
   {
     id: 0,
-    heading: 'Profesional Art Printing Data',
+    name: 'Profesional Art Printing Data',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     image: './images/probackground.png',
     button: ['see live', 'see source', 'see project'],
@@ -61,7 +61,7 @@ const proCards = [
   },
   {
     id: 4,
-    heading: 'Multi-post stories',
+    name: 'Multi-post stories',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     image: './images/probackground.png',
     button: ['see live', 'see source', 'see project'],
@@ -71,7 +71,7 @@ const proCards = [
   },
   {
     id: 5,
-    heading: 'Multi-post stories',
+    name: 'Multi-post stories',
     image: './images/probackground.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: ['see live', 'see source', 'see project'],
@@ -79,7 +79,7 @@ const proCards = [
   },
   {
     id: 6,
-    heading: 'Multi-post stories',
+    name: 'Multi-post stories',
     image: './images/probackground.png',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: ['see live', 'see source', 'see project'],
@@ -120,89 +120,6 @@ const popCards = [
   },
 ];
 
-// popup
-
-const popupVar = document.querySelector('.recent-div');
-
-for (let i = 0; i <= (document.querySelectorAll('.click')).length; i += 1) {
-  const popCard = `
-  <div id="my-modal" class="modal">
-  <div class="modal-content">
-    <div class="modal-header">
-    <h3 class="modal-head-text">${proCards[i].heading}</h3>
-    <h3 id="heading-popup" class="modal-head-text-desk">${proCards[i].heading}</h3>
-      <span>
-      <img src=images/vectorx.png alt="project image" class="close" id="popclose"/>
-      </span>
-    </div>
-    <ul class="tech-button1 pop-tech-but">
-    <li class="pop-card1">
-      <nav class="icon-1">${popCards[i].technologies[0]}</nav>
-    </li>
-    <li class="pop-card2">
-    <nav class="icon-1">${popCards[i].technologies[1]}</nav>
-    </li>
-    <li class="pop-card3">
-    <nav class="icon-1">${popCards[i].technologies[2]}</nav>
-    </li>
-    <li class="pop-card4">
-    <nav class="icon-1">${popCards[i].technologies[2]}</nav>
-    </li>
-    </ul>
-    <div class="modal-body">
-    <img src=images/popup-img.png alt="project image" class="pop-window"/>
-    <img src=images/popup-desktop-img.png alt="project image" class="pop-window-desk"/>
-    <p class="pop-text"> ${popCards[0].description}
-    <p class="pop-text-desk"> ${popCards[1].description}
-    </div>
-    <div class="modal-footer">
-    <div class="pop-btn">
-    <button class="link-button1 ltext1" type="button">
-    <a class="seelive-but" href=${popCards[0].liveLink}>${proCards[i].button[0]}<img class="btn-icon1" src="./images/seelive.png"/>
-    </a>
-    </button>
-    <button class="link-button2 ltext1" type="button">
-    <a class="seesrc-but" href=${popCards[0].sourceLink}>${proCards[i].button[1]}
-    <img class="btn-icon2" src="./images/seesource.png"/>
-    </a>
-    </button>
-    </div>
-    </div>
-  </div>
-</div>
-`;
-
-  popupVar.innerHTML += popCard;
-}
-
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#project-button');
-const closeBtn = document.querySelector('#popclose');
-
-// Open
-function openModal() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closeModal() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
-
-// pop-end
-
 const projectSection1 = document.querySelector('#project-section');
 
 for (let i = 1; i <= 1; i += 1) {
@@ -215,7 +132,7 @@ for (let i = 1; i <= 1; i += 1) {
   <div/>
 </div>
 <div class="pro1">
-<h2 class="ptext1">${proCards[0].heading}</h2>
+<h2 class="ptext1">${proCards[0].name}</h2>
 <p class="ptext-1"> ${proCards[i].description}
 </p>
 <ul class="tech-button1">
@@ -229,7 +146,7 @@ for (let i = 1; i <= 1; i += 1) {
   <nav class="icon-1">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop" class="pop-up but1 butext but click" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop" class="pop-up but1 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection1.innerHTML += card;
@@ -248,7 +165,7 @@ for (let i = 1; i <= 1; i += 1) {
 <div class="pro-img2-desk">
   <div/>
 <div class="pro2">
-<h2 class="ptext2">${proCards[0].heading}</h2>
+<h2 class="ptext2">${proCards[0].name}</h2>
 <p class="ptext-2"> ${proCards[i].description}
 </p>
 <ul class="tech-button2">
@@ -262,7 +179,7 @@ for (let i = 1; i <= 1; i += 1) {
   <nav class="icon-2">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop2" class="pop-up but2 butext click but" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop2" class="pop-up but2 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection2.innerHTML += card;
@@ -281,7 +198,7 @@ for (let i = 2; i <= 2; i += 1) {
 <div class="pro-img3-desk">
   <div/>
 <div class="pro3">
-<h2 class="ptext3">${proCards[0].heading}</h2>
+<h2 class="ptext3">${proCards[0].name}</h2>
 <p class="ptext-3"> ${proCards[i].description}
 </p>
 <ul class="tech-button3">
@@ -295,7 +212,7 @@ for (let i = 2; i <= 2; i += 1) {
   <nav class="icon-3">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop3" class="pop-up but3 butext click but" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop3" class="pop-up but3 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection3.innerHTML += card;
@@ -312,7 +229,7 @@ for (let i = 3; i <= 3; i += 1) {
   alt="project card four"
 />
 <div class="pro4">
-<h2 class="ptext4">${proCards[0].heading}</h2>
+<h2 class="ptext4">${proCards[0].name}</h2>
 <p class="ptext-4"> ${proCards[i].description}
 </p>
 <ul class="tech-button4">
@@ -326,7 +243,7 @@ for (let i = 3; i <= 3; i += 1) {
   <nav class="icon-4">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop4" class="pop-up but4 butext click but" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop4" class="pop-up but4 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection4.innerHTML += card;
@@ -345,7 +262,7 @@ for (let i = 4; i <= 4; i += 1) {
 <div class="pro-img5-desk">
   <div/>
 <div class="pro5">
-<h2 class="ptext5">${proCards[0].heading}</h2>
+<h2 class="ptext5">${proCards[0].name}</h2>
 <p class="ptext-5"> ${proCards[i].description}
 </p>
 <ul class="tech-button5">
@@ -359,7 +276,7 @@ for (let i = 4; i <= 4; i += 1) {
   <nav class="icon-5">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop5" class="pop-up but5 butext click but" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop5" class="pop-up but5 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection5.innerHTML += card;
@@ -378,7 +295,7 @@ for (let i = 5; i <= 5; i += 1) {
 <div class="pro-img6-desk">
   <div/>
 <div class="pro6">
-<h2 class="ptext6">${proCards[0].heading}</h2>
+<h2 class="ptext6">${proCards[0].name}</h2>
 <p class="ptext-6"> ${proCards[i].description}
 </p>
 <ul class="tech-button6">
@@ -392,171 +309,8 @@ for (let i = 5; i <= 5; i += 1) {
   <nav class="icon-6">${proCards[i].technologies[2]}</nav>
   </li>
   </ul>
-  <button id="see-pro-pop6" class="pop-up but6 butext click but" type="button"> ${proCards[i].button[2]} </button>
+  <button id="see-pro-pop6" class="pop-up but6 butext but" type="button"> ${proCards[i].button[2]} </button>
   </div>`;
 
   projectSection6.innerHTML += card;
 }
-
-// pop up for cards 1
-
-const popUpBtn = document.querySelector('#see-pro-pop');
-const closePop = document.querySelector('#popclose');
-
-// Open
-function openModal1() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick1(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn.addEventListener('click', openModal1);
-closePop.addEventListener('click', closepopmodal);
-window.addEventListener('click', outsideClick1);
-
-// pop up for cards 2
-
-const popUpBtn2 = document.querySelector('#see-pro-pop2');
-const closePop2 = document.querySelector('#popclose');
-
-// Open
-function openModal2() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal2() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick2(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn2.addEventListener('click', openModal2);
-closePop2.addEventListener('click', closepopmodal2);
-window.addEventListener('click', outsideClick2);
-
-// pop up for cards 3
-
-const popUpBtn3 = document.querySelector('#see-pro-pop3');
-const closePop3 = document.querySelector('#popclose');
-
-// Open
-function openModal3() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal3() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick3(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn3.addEventListener('click', openModal3);
-closePop3.addEventListener('click', closepopmodal3);
-window.addEventListener('click', outsideClick3);
-
-// pop up for cards 4
-
-const popUpBtn4 = document.querySelector('#see-pro-pop4');
-const closePop4 = document.querySelector('#popclose');
-
-// Open
-
-function openModal4() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal4() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick4(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn4.addEventListener('click', openModal4);
-closePop4.addEventListener('click', closepopmodal4);
-window.addEventListener('click', outsideClick4);
-
-// pop up for cards 5
-
-const popUpBtn5 = document.querySelector('#see-pro-pop5');
-const closePop5 = document.querySelector('#popclose');
-
-// Open
-function openModal5() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal5() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick5(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn5.addEventListener('click', openModal5);
-closePop5.addEventListener('click', closepopmodal5);
-window.addEventListener('click', outsideClick5);
-
-// pop up for cards 6
-
-const popUpBtn6 = document.querySelector('#see-pro-pop6');
-const closePop6 = document.querySelector('#popclose');
-
-// Open
-function openModal6() {
-  modal.style.display = 'block';
-}
-
-// Close
-function closepopmodal6() {
-  modal.style.display = 'none';
-}
-
-// Close If Outside Click
-function outsideClick6(e) {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-}
-
-// Events
-popUpBtn6.addEventListener('click', openModal6);
-closePop6.addEventListener('click', closepopmodal6);
-window.addEventListener('click', outsideClick6);
